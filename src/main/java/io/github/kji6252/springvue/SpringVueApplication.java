656329a6d2bcec812c80a4e1038e0bbdee97248c
[상품개발팀] 김종인 <jongin.kim01@lotte.net>
2022-07-19 16:53:29 +0900

    Init Project

---
+++ src/main/java/io/github/kji6252/springvue/SpringVueApplication.java
@@ -0,0 +1,13 @@
+package io.github.kji6252.springvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kji6252/springvue/SpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kji6252.springvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
