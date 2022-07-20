78d98e196d3caf6fc0dffc968c326df18a537a44
JonginKim <kji6252@naver.com>
2022-07-20 11:29:21 +0900

    Init Vue

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project Setup
 1. [Spring Init](https://start.spring.io)
+2. [Vue CLI](https://cli.vuejs.org/)
+3. [gradle-node-plugin](https://github.com/node-gradle/gradle-node-plugin/blob/3.4.0/docs/usage.md)
---
+++ src/main/java/io/github/kji6252/springvue/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package io.github.kji6252.springvue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests(authorize -> authorize
+                        .mvcMatchers("/**").permitAll()
+                        .mvcMatchers("/api/**").authenticated()
+                );
+        return http.build();
+    }
+}
