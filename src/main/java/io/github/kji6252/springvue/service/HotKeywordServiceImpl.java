b6051a4cf6be985d71570fa9750e9274ebbc5ef3
JonginKim <kji6252@naver.com>
2022-08-08 08:18:51 +0900

    Add Resilience4j Circuit Breaker

--- src/main/java/io/github/kji6252/springvue/service/HotKeywordServiceImpl.java
+++ src/main/java/io/github/kji6252/springvue/service/HotKeywordServiceImpl.java
@@ -35,7 +35,7 @@ public class HotKeywordServiceImpl implements HotKeywordService {
         hotKeywordRepository.save(hotKeyword);
     }
 
-//    @Cacheable(value = "top10HotKeywords", sync = true)
+    @Cacheable(value = "top10HotKeywords", sync = true)
     @Transactional(readOnly = true)
     @Override
     public List<HotKeyword> getTop10HotKeyword() {
